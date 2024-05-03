a7ee7b986d08bfb0f65d0acc8268741f0779c4ad
zoya <112046214+zoyaumar@users.noreply.github.com>
2024-05-03 18:13:33 -0400

    Update FindOdd.java

--- FindOdd.java
+++ FindOdd.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class FindOdd {
-	public static int findIt(int[] a) {
+  public static int findIt(int[] a) {
     for(int n : a)
       {
         if(Arrays.stream(a).filter(d -> d == n).count()%2==1)
