974b26e6109a9e61415cec49477b4e05c630dc60
zoya <112046214+zoyaumar@users.noreply.github.com>
2024-05-17 18:46:57 -0400

    Create TwoSum.java
    Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple

---
+++ TwoSum.java
@@ -0,0 +1,13 @@
+public class TwoSum
+{
+    public static int[] twoSum(int[] numbers, int target)
+    {
+        for (int i=0; i<numbers.length;i++) {
+            for (int j=1; j<numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target)
+                    return new int[]{i,j};
+            }
+        }
+        return null;
+    }
+}
