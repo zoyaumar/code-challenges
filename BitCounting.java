3a2c97a8ada60d2bc45fad03f1565f52c3c784c0
zoya <112046214+zoyaumar@users.noreply.github.com>
2024-05-20 20:52:54 -0400

    Update BitCounting.java

--- BitCounting.java
+++ BitCounting.java
@@ -1,7 +1,7 @@
 public class BitCounting {
 	public static int countBits(int n){
 		// return Integer.toBinaryString(n).replace("0","").length();
-    // OR
-    return Integer.bitCount(n);
+    		// OR
+    		return Integer.bitCount(n);
 	}
 }
