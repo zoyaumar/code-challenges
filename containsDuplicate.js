acd9a8ef166fcf9df0bd6dbebed0851f0f6d4006
zoya <112046214+zoyaumar@users.noreply.github.com>
2024-11-21 20:26:26 -0500

    Update containsDuplicate.js

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -3,5 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    return nums.length != nums.filter((el, index, arr) => index == arr.indexOf(el)).length
+    // return nums.length != nums.filter((el, index, arr) => index == arr.indexOf(el)).length
+    return new Set(nums).size != nums.length
 };
