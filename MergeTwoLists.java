9f81367f69a9f7ea9ba91c02240c00c8440a43eb
Zoya Umar <112046214+zoyaumar@users.noreply.github.com>
2025-06-11 23:11:41 -0400

    Create MergeTwoLists.java

---
+++ MergeTwoLists.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode();
+        ListNode merged = head;
+        
+        while(list1!=null&&list2!=null){
+            if(list1.val>list2.val){
+                merged.next = list2;   
+                list2=list2.next; 
+            }
+            else{
+                merged.next = list1;   
+                list1=list1.next; 
+            }
+            merged=merged.next;
+        }
+
+        if(list1!=null)
+            merged.next = list1;
+        if(list2!=null)
+            merged.next = list2;
+
+        return head.next;
+    }
+}
