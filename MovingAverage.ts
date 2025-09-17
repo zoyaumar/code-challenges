895e9e7bee658c221f5a6692b2a8007e145dee4b
Zoya Umar <112046214+zoyaumar@users.noreply.github.com>
2025-09-17 01:49:11 -0400

    Create MovingAverage.ts

---
+++ MovingAverage.ts
@@ -0,0 +1,25 @@
+class MovingAverage {
+    private size: number;
+    private nums:number[] = []
+    private sum:number = 0
+
+    constructor(size: number){
+        this.size = size
+    }
+
+    next(val: number): number {
+        if(this.nums.length >= this.size)
+            this.nums.shift()
+        this.nums.push(val)
+
+        this.sum = this.nums.reduce((a:number,b:number) => (b + a), 0)
+        let average = this.sum/this.nums.length
+        return average
+    }
+}
+
+const ma = new MovingAverage(3);
+console.log(ma.next(1)) //1
+console.log(ma.next(10)) //5.5
+console.log(ma.next(4)) // 5
+console.log(ma.next(4)) // 6
